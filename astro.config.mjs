baabb07dce1cbe0ec86f8d1efe5187fcf85a24a3
Ted Vu <tedvu184@gmail.com>
2025-05-15 22:29:12 +1000

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import vue from '@astrojs/vue';
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), vue()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), vue()],
+  output: "static",
 });
