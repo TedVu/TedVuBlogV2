e422a69dc64bdf18db80b5d18603c049d563411f
Ted Vu <tedvu184@gmail.com>
2026-07-20 00:32:22 +1000

    installing fusejs and setting up search engine

---
+++ src/utilities/search-engine.ts
@@ -0,0 +1,27 @@
+import Fuse, { type IFuseOptions } from 'fuse.js';
+import type { BlogPost } from '../lib/contentful';
+
+let fuse: Fuse<BlogPost> | null = null;
+export async function getSearchEngine(): Promise<Fuse<BlogPost>> {
+
+    if(fuse){
+        return fuse;
+    }
+
+    const response = await fetch('/search-index.json');
+
+    if(!response.ok){
+        throw new Error(`Failed to fetch search index: ${response.statusText}`);
+    }
+
+    const searchIndex: BlogPost[] = await response.json();
+
+    const options: IFuseOptions<BlogPost> = {
+        keys: ['title', 'body'],
+        includeScore: true,
+        threshold: 0.3,
+    };
+
+    fuse = new Fuse(searchIndex, options);
+    return fuse;
+}
